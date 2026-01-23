8f1ad9633d1947e65b7de4efab65a091eecc9207
Gabrielfonseca1008 <gfcruz06@gmail.com>
2026-01-22 22:46:36 -0300

    reestruturei com loop 2

--- Ranking.js
+++ Ranking.js
@@ -1,7 +1,7 @@
 // Coloque o nome do heróis nas apsas
 let heroName = " John Wick, o Babayaga" 
 //Nivel de experiência do herói
-let xp = 6000
+let xp = 950
 
 //Classificações de cada nível
 let classe = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Mestre", "Imortal", "Radiante"]
@@ -9,8 +9,12 @@ let classe = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Mest
 let niveisXp = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000]
 
 for(let i = 0; i < niveisXp.length; i++){
-    if(xp <= classe[i]){
+    if(xp <= niveisXp[i]){
         console.log("O herói de nome " + heroName + "está no nível " + classe[i])
+        break
+    }else if (xp >= niveisXp[7]){
+        console.log("O herói de nome" + heroName + "está no nível " + classe[8])
+        break
     }
 }
 
